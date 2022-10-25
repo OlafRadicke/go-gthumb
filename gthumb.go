3066985a41431b48c1ba589a4d78c835f6bea0bf
Olaf Radicke <briefkasten@olaf-radicke.de>
2022-10-25 22:46:05 +0200

    WIP

--- gthumb.go
+++ gthumb.go
@@ -1,9 +1,29 @@
 package gthumb
 
-import "log"
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 // GthumbCommentsRead Read an file from the .comment directory.
-func GthumbCommentsRead(path string) {
+func GthumbCommentsRead(path string) (comment *XMLComment, err error) {
 
+	var comment XMLComment
 	log.Panicf("Try to read ", path)
+
+	xmlFile, err := os.Open(mediaInfo.AbsoluteManifestSourcePath)
+	if err != nil {
+		return nil, fmt.Errorf("can't read comment file: %s", err)
+	}
+	defer xmlFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+	if err := xml.Unmarshal(byteValue, &comment); err != nil {
+		return nil, fmt.Errorf("can't unmarshal comment file: %s", err)
+	}
+
+	return &comment
 }
